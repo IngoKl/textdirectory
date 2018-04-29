ddf6292599b27c767cfc071fea9d4f99e1cf8f3d
Ingo Kleiber <kleiber@heiedu.uni-heidelberg.de>
2018-04-30 01:13:58 +0200

    import

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import textdirectory
+#import textdirectory
 
 # -- General configuration ---------------------------------------------
 
