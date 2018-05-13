6d99e61fafdb7681c35cf28d9483e6dd70e0eb93
Ingo Kleiber <kleiber@heiedu.uni-heidelberg.de>
2018-05-13 16:42:18 +0200

    bumpversion

--- textdirectory/__init__.py
+++ textdirectory/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Ingo Kleiber"""
 __email__ = 'ingo@kleiber.me'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from textdirectory import *
 from textdirectory.textdirectory import *
