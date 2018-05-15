7418bb37e71970cafa62a59efbfe37864b49fa0b
Ingo Kleiber <kleiber@heiedu.uni-heidelberg.de>
2018-05-16 01:53:59 +0200

    example.py

--- example.py
+++ example.py
@@ -5,11 +5,6 @@ td.load_files(True, 'txt')
 
 td.filter_by_max_chars(480)
 td.stage_transformation(['transformation_to_leetspeak'])
-td.filter_by_min_chars(14)
 td.print_pipeline()
-td.load_aggregation_state()
-td.print_pipeline()
-td.print_saved_states()
-
 
 td.transform_to_memory()
